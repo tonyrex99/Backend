f9739cd652babff3f00b19b9bd179af4d9ffba45
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-12-21 12:07:46 +0100

    useing sqlite again

--- config/database.js
+++ config/database.js
@@ -1,14 +1,15 @@
+const path = require("path");
+
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("DATABASE_HOST", "https://viaduct.proxy.rlwy.net"),
-      port: env.int("DATABASE_PORT", 29927),
-      database: env("DATABASE_NAME", "railway"),
-      username: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "AE*ggeDea2d6a-gcdA1BD3dbff*A-gEE"),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/database.db")
+      ),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
