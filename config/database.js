79c371926b7528505601fcaf7c461a1934f69b72
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-12-21 21:51:47 +0100

    fix db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const path = require("path");
-
-module.exports = ({ env }) => ({
+/** module.exports = ({ env }) => ({
   connection: {
     client: "sqlite",
     connection: {
@@ -11,5 +10,24 @@ module.exports = ({ env }) => ({
       ),
     },
     useNullAsDefault: true,
+  },
+});
+
+*/
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "mysql",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
+    },
+    debug: false,
   },
 });
