7c04704f2c946b056e4ecc4e42d48201f02a5716
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-12-21 09:08:36 +0100

    production configs

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
