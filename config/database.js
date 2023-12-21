b589ad44d0d6632e524ce81f4206e7fe91f8e24f
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-12-21 09:48:55 +0100

    retry db

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 29927),
-      database: env("DATABASE_NAME", "railway"),
-      username: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "0000"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      username: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       schema: env("DATABASE_SCHEMA", "public"),
     },
   },
